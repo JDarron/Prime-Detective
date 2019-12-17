c0f2f7bd303a7b8f5266e65f25900f92e8a5ffaf
Joshua Spears <j.dspears@yahoo.com>
2019-12-17 11:27:28 -0800

    Fixed slight styling issues.

--- public/assets/javascript/main.js
+++ public/assets/javascript/main.js
@@ -1,29 +1,35 @@
 $(document).ready(function () {
     const instance = M.Modal.getInstance($('.modal').modal())
+    const form = $('#number-form')
+    const modal = $('#modal')
+    const modalTxt = $('#modal-txt')
+    const modalFooter = $('.modal-footer')
 
 
-    $("#number-form").submit(function (e) {
+    form.submit(function (e) {
         e.preventDefault()
 
         const number = $('#number').val()
         this.reset()
 
         $.ajax({
-            url: "/detect",
-            type: "POST",
+            url: '/detect',
+            type: 'POST',
             data: {
                 number: number
             },
 
             success: function (res) {
                 if (res.prime) {
-                    $('#modal-msg').text('Guilty of being a prime')
-                    $('#modal').removeClass('red')
-                    $('#modal').addClass('green')
+                    modalTxt.text('Guilty of being a prime')
+                    modal.removeClass('red')
+                    modal.addClass('green')
+                    modalFooter.css('background-color', '#e8f5e9')
                 } else {
-                    $('#modal-msg').text('Inocent. Not a prime')
-                    $('#modal').removeClass('green')
-                    $('#modal').addClass('red')
+                    modalTxt.text('Inocent. Not a prime')
+                    modal.removeClass('green')
+                    modal.addClass('red')
+                    modalFooter.css('background-color', '#ffebee')
                 }
 
                 instance.open()
@@ -33,5 +39,5 @@ $(document).ready(function () {
                 console.error(err)
             }
         })
-    });
-});
+    })
+})
